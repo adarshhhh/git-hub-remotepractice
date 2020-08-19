dcbd56543be7d14eec8f78e1adc108835da2d36a
adarsh vishwakarma <adarsh.cool.31@gmail.com>
2020-08-19 08:29:03 +0530

    nevigation command added

--- github/git-hub-practice1.py
+++ github/git-hub-practice1.py
@@ -13,3 +13,27 @@ print(driver.page_source)  # HTML code for the page
 
 driver.find_element_by_xpath("//*[@id='nav-xshop']/a[2]").click()
 time.sleep(5)
+
+# NEVIGATION COMMANDS
+
+#from selenium import webdriver
+#from selenium.webdriver.common.keys import Keys
+#import time
+#driver = webdriver.Chrome(executable_path="C:\\Users\Archi\PycharmProject\chrome driver\chromedriver")
+
+#driver.get("https://app.slack.com/client/T0B1VAM0Q/DTY0RQ89G?cdn_fallback=2")
+#print(driver.title)
+
+#driver.get("https://en-gb.facebook.com/login/")
+#time.sleep(5)
+#print(driver.title)
+
+#driver.back()      #to go back
+#time.sleep(5)
+#print(driver.title)
+
+#driver.forward()   #to go forword
+#time.sleep(5)
+#print(driver.title)
+
+#driver.close()
